c5a426dc35e5da2e6351a26634129cc49c06619d
Suhrith <84776369+suhrith13@users.noreply.github.com>
2026-01-20 18:30:35 -0500

    Update find-minimum-in-rotated-sorted-array.py

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,3 +1,8 @@
+take a array and designate left pointer and right pointer ( indexes -> 0 and n-1)
+while the left pointer is smaller than right pointer calculate the mid point 
+if mid point is less that or equal to right pointer it means that the min element is either the mid point or on the left side so discard the right side elements, update the right pointer to mid element
+if thats not the case move the mid pointer to the right, once you find the min element L == R, then return the element.
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
