104ade98ac8b56e17353a55c88b14161b2aedac5
Suhrith <84776369+suhrith13@users.noreply.github.com>
2026-01-20 18:46:57 -0500

    Update search-in-rotated-sorted-array.py

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,3 +1,8 @@
+once you initialise and fint the mid point, if the target is the mid point reutrn it. 
+Now we need to make sure left half is sorted(it may or may not be sorted). Once it is sorted, if the target is btw left and mid point we search from right to mid -1, if not then we search from left to mid + 1
+Now we make sure right half is sorted, then we shorten the search from left to mid + 1 and right to mid - 1.
+if the target is not found in any sides we return -1 as per the qs.
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
