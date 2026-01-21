66ff6c1027b134f121fad5ddb592b81d75430b8e
Suhrith <84776369+suhrith13@users.noreply.github.com>
2026-01-20 19:26:46 -0500

    Update time-based-key-value-store.py

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,3 +1,8 @@
+first we initialise a dictionary to store key- value pairs. If the key does not exist in self.dic, initialize it with an empty list.Append the [value, timestamp] pair to the list associated with the key in self.dic.
+now we retireive the key value pairs from self.dic with the key value, then we use binary search to search the latest  value which is less than or equal to the given timestamp vallue.
+initialise l, r to 0 and index - 1 and calculate the mid index. Now we check If the timestamp at mid is less than or equal to the given timestamp, update res with the value at mid and adjust l to search the right half of the list.
+Otherwise, adjust r to search the left half of the list. finally we return res
+
 class TimeMap:
 
     def __init__(self):
